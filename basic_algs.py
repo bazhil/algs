8cf21793279743b08fe537a9d80dcdc18227d12c
bazhil <i-bazh@mail.ru>
2021-09-29 23:36:27 +0300

    Finish chapter one

--- basic_algs.py
+++ basic_algs.py
@@ -50,10 +50,65 @@ def contain_duplicates_2(arr):
     :param arr: список натуральных чисел
     :return:
     """
-    return True if len(arr) > len(set(arr)) else False
+    return bool(len(arr) > len(set(arr)))
+
+
+def contain_duplicates_3(arr):
+    """
+    Очередной метод, определяющий, содержит ли список дубликаты
+    :param arr: список натуральных чисел
+    :return:
+    """
+    for k, v in enumerate(arr):
+        if k == len(arr) - 1:
+            return False
+        if v == arr[k+1]:
+            return True
+
+
+def dividing_point(arr):
+    """
+    метод, который грубо определяет среднее значение в массиве.
+    работает за O(1)
+    :param arr: список натуральных чисел
+    :return:
+    """
+    n1 = arr[0]
+    n2 = arr[-1]
+    n3 = arr[arr.index(n2)//2]
+    if n1 < n2 and n2 < n3:
+        return n1
+    if n2 < n1 and n3 < n3:
+        return n2
+    return n3
+
+
+# def find_item(target_value):
+#     """
+#     Метод, который ищет значение в куче / дереве
+#     :param target_value: искомое значение
+#     :return:
+#     """
+#     # TODO: позже составить дерево и проверить как работает алгоритм -> разобраться со свойствами
+#     test_node = 0
+#
+#     while True:
+#         if test_node == None:
+#             return
+#         if target_value == test_node.Value():
+#             return test_node
+#         elif target_value < test_node.Value():
+#             test_node.LeftChild()
+#         else:
+#             test_node = test_node.RightChild()
 
 
 if __name__ == '__main__':
+    arr = [1, 2, 3, 4, 6, 7, 5, 5, 5, 6, 8, 10]
+    arr2 = [1, 2, 3, 4, 6, 7, 5, 5]
     # print(GCD(1220, 5))
     # print(find_largest(list(range(15))))
-    print(contain_duplicates_2([1, 2, 3, 4, 6, 7, 5, 5]))
+    # print(contain_duplicates_2(arr2))
+    print(contain_duplicates_3(arr2))
+    # print(dividing_point(arr))
+    # print(dividing_point(arr2))
