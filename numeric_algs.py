7c6cc51f9c0385db4a80b2cc44723febe5c44d0f
bazhil <i-bazh@mail.ru>
2021-10-06 22:31:20 +0300

    finish chapter about numeric algs

--- numeric_algs.py
+++ numeric_algs.py
@@ -111,6 +111,128 @@ def find_primes_2(num: int, tests: int):
             yield i
 
 
+def use_rectangle_rule(func, xmin, xmax, num_intervals):
+    """
+    Функция, реализующая формулу прямоугольников, для нахождения площади под графиком функции.
+    :param func: функция
+    :param xmin: минимальное значение параметра
+    :param xmax: максимальное значение параметра
+    :param num_intervals: количество интервалов (чем больше, тем меньше погрешность)
+    :return:
+    """
+    # вычисляем ширину прямоугольника
+    dx = (xmax - xmin) / num_intervals
+
+    # добавляем области прямоугольников
+    total_area = 0
+    x = xmin
+    i = 1
+    while i <= num_intervals:
+        total_area += dx * func(x)
+        x += dx
+        i += 1
+
+    return total_area
+
+
+def trapezoid_rule(func, xmin, xmax, num_intervals):
+    """
+    Функция, использующая формулу трапеции
+    :param func: функция
+    :param xmin: минимальное значение параметра
+    :param xmax: максимальное значение параметра
+    :param num_intervals: количество интервалов
+    :return:
+    """
+    # вычисляем ширину трапеции
+    dx = (xmax - xmin) / num_intervals
+
+    total_area = 0
+    x = xmin
+    i = 1
+    while i <= num_intervals:
+        total_area += dx * (func(x) + func(x + dx)) / 2
+        x += dx
+        i += 1
+
+    return total_area
+
+
+def slice_area(func, x1, x2, max_slice_error):
+    """
+    Возвращает площадь заданной области
+    :param func: функция
+    :param x1: значение параметра
+    :param x2: значение параметра
+    :param max_slice_error:
+    :return: максимальная ошибка
+    """
+    # вычисляем значение функции на конечных и центральной точках
+    y1 = func(x1)
+    y2 = func(x2)
+    xm = (x1 + x2) / 2
+    ym = func(xm)
+
+    # вычисляем площадь большой трапеции и двух меньших трапеций
+    area12 = (x2 - x1) * (y1 + y2) / 2
+    area1m = (xm - x1) * (y1 + ym) / 2
+    aream2 = (x2 - xm) * (ym + y2) / 2
+    area1m2 = area1m + aream2
+
+    error = (area1m2 - area12) / area12
+
+    # если погрешность приемлемая возвращаем полученный результат
+    if abs(error) < max_slice_error:
+        return area1m2
+
+    # делим трапецию и делим еще раз
+    return slice_area(func, x1, xm, max_slice_error) + slice_area(func, xm, x2, max_slice_error)
+
+
+def adaprive_integrate_midpoint(func, xmin, xmax, num_intervals, max_slice_error):
+    """
+    Функция, использующая адаптивную формулу трапеции
+    :param func: функция
+    :param xmin: минимальное значение параметра
+    :param xmax: максимальное значение параметра
+    :param num_intervals: количество интервалов
+    :param max_slice_error: максимальная ошибка
+    :return:
+    """
+    # вычисляем ширину трапеции
+    dx = (xmax - xmin) / num_intervals
+
+    total_area = 0
+    x = xmin
+    i = 1
+    while i <= num_intervals:
+        total_area += slice_area(func, x, x+dx, max_slice_error)
+        x += dx
+        i += 1
+
+    return total_area
+
+
+def find_zero(func, dfdx, initial_guess, max_error):
+    """
+    Метод Ньютона - поиск пересечения оси абцисс с помощью касетельных
+    :param func: функция
+    :param dfdx: производная функции
+    :param initial_guess: начальное приближение
+    :param max_error: максимальная ошибка
+    :return:
+    """
+    x = initial_guess
+
+    i = 0
+    while i < 100:
+        y = func(x)
+        if abs(y) < max_error:
+            break
+        x = x - y / dfdx(x)
+        i += 1
+
+    return x
 
 
 if __name__ == '__main__':
@@ -118,5 +240,12 @@ if __name__ == '__main__':
     # print(find_factor(5122))
     # print(find_primes(56))
     # print(is_primitive(710, 10))
-    for i in find_primes_2(13, 10):
-        print(i)
+
+    # for i in find_primes_2(13, 10):
+    #     print(i)
+
+    # print(use_rectangle_rule(lambda x: 12*x, 0, 5, 10))
+    # print(trapezoid_rule(lambda x: 12*x, 0, 5, 10))
+    # print(adaprive_integrate_midpoint(lambda x: 12*x, 0, 5, 5, 1))
+
+
