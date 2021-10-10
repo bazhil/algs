5da4a271ebf0ccacd603aa1c4b8edb893f6990d2
bazhil <i-bazh@mail.ru>
2021-10-10 22:25:07 +0300

    Add new simple alg

--- lists_algs.py
+++ lists_algs.py
@@ -13,5 +13,19 @@ def iterate(lst):
             top = lst[i+1]
 
 
+def find_cell_before(lst: list, value):
+    """
+    Алгоритм, который находит номер ячейки, идущей перед ячейкой в которой лежит переданное в данный метод значение
+    :return:
+    """
+    if value not in lst:
+        return None
+    i = 0
+    while lst[i] != None:
+        if lst[i] == value:
+            return lst.index(value)
+        i += 1
+
 if __name__ == '__main__':
-    iterate([1,2,3,4,5])
+    # iterate([1,2,3,4,5])
+    print(find_cell_before([123,22,1,2,3,19,4,55,5,6,7,8,9], 1))
