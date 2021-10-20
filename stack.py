648dca167045b24b49c0d7efa4725dc66177f328
bazhil <i-bazh@mail.ru>
2021-10-20 22:56:26 +0300

    Add another sorting

--- stack.py
+++ stack.py
@@ -65,17 +65,74 @@ class DoubleStack(Stack):
         return self.values.pop(0)
 
 
+def hanoi_tower(first_tower: Stack, second_tower: Stack, third_tower: Stack) -> Stack:
+    """
+    Реализация решения задачи Ханойская башня
+    Выполняется за (2**N - 1) шагов
+    :param first_tower: первая башня (стек)
+    :param second_tower: вторая башня (стек)
+    :param third_tower: третья башня (стек)
+    :return:
+    """
+    second_tower.push(first_tower.pop())
+    third_tower.push(first_tower.pop())
+    third_tower.push(second_tower.pop())
+    second_tower.push(first_tower.pop())
+    first_tower.push(third_tower.pop())
+    second_tower.push(third_tower.pop())
+    second_tower.push(first_tower.pop())
+
+    return second_tower
+
+
+def insertion_sort(items: list) -> list:
+    """
+    Сортировка вставкой
+    :param items: неотсортированный стек
+    :return:
+    """
+    num_items = len(items)
+    for i in range(1, num_items):
+        next_item = items[i]
+        j = i - 1
+        while j >= 0 and next_item < items[j]:
+            items[j + 1] = items[j]
+            j -= 1
+
+        items[j + 1] = next_item
+
+    return items
+
+
 if __name__ == '__main__':
+    # TODO: оформить примеры в тесты с использованием библиотек/фреймворков
     # st = Stack()
     # for i in range(5):
     #     st.push(i)
     # for i in range(6):
     #     print(st.pop())
 
-    ds = DoubleStack()
-    for i in range(5):
-        ds.push(i)
-        ds.push_in_start(i+10)
-    for i in range(6):
-        print(ds.pop_from_start(), ds.pop())
+    # ds = DoubleStack()
+    # for i in range(5):
+    #     ds.push(i)
+    #     ds.push_in_start(i+10)
+    # for i in range(6):
+    #     print(ds.pop_from_start(), ds.pop())
+
+    # first, second, third = Stack(), Stack(), Stack()
+    # for i in [3, 2, 1]:
+    #     first.push(i)
+    #
+    # second = hanoi_tower(first, second, third)
+    # for i in range(4):
+    #     print(second.pop())
+
+    st = Stack()
+    test_data = [9, 2, 6, 10, 3, 1]
+    for i in test_data:
+        st.push(i)
+    print(f'Test data: {test_data}')
+    sorted_data = insertion_sort(test_data)
+    print(f'Sorted data: {sorted_data}')
+
 
