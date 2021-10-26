42910d4795df3b1934ecc6b302157be9e48610cb
bazhil <i-bazh@mail.ru>
2021-10-26 22:46:59 +0300

    Add bubble sort

--- sortings.py
+++ sortings.py
@@ -15,8 +15,31 @@ def insertion_sort(values):
     return values
 
 
+def bubble_sort(values):
+    """
+    Пузырьковая сортировка O(N**2)
+    :param values: список чисел
+    :return:
+    """
+    not_sorted = True
+    while not_sorted:
+        not_sorted = False
+        for i in range(1, len(values)):
+            if values[i] < values[i-1]:
+                values[i], values[i-1] = values[i-1], values[i]
+                not_sorted = True
+
+    return values
+
+
+
+
 if __name__ == '__main__':
     test_list = [9, 8, 55, 7, 2, 0, 568, 3, 4, 11]
 
-    lst = insertion_sort(test_list)
-    print(lst)
+    # ins_lst = insertion_sort(test_list)
+    # print(ins_lst)
+    bbl_lst = bubble_sort(test_list)
+
+    print(bbl_lst)
+
