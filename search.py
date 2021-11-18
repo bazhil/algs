452e1c10916f6a823e86d5f32f2abad8ac0d0d26
bazhil <i-bazh@mail.ru>
2021-11-18 22:43:21 +0300

    Add binary search

--- search.py
+++ search.py
@@ -15,3 +15,31 @@ def linear_search(values: list, target) -> int:
             return -1
     return -1
 
+
+def binary_search(values: list, target) -> int:
+    """
+    Бинарный поиск
+    :param values: список значений
+    :param target: искомое значение
+    :return:
+    """
+    min = 0
+    max = len(values) - 1
+
+    while min < max:
+        mid = (max + min) // 2
+        if target < values[mid]:
+            max = mid - 1
+        elif target > values[mid]:
+            min = mid + 1
+        else:
+            return mid
+    return -1
+
+
+if __name__ == '__main__':
+    test_list = [i for i in list(range(12324342)) if i % 17 == 0]
+    ind1 = linear_search(test_list, 34)
+    print(ind1, test_list[ind1])
+    ind2 = binary_search(test_list, 170)
+    print(ind2, test_list[ind2])
