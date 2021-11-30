523042fb011642e47182960114eed8b7c27f800d
bazhil <i-bazh@mail.ru>
2021-12-01 00:18:04 +0300

    Add graphical algs

--- graphical_algs.py
+++ graphical_algs.py
@@ -6,15 +6,6 @@ import turtle
 
 t = turtle.Turtle()
 
-def draw_koch_segment(t, ln):
-    t.fd(ln)
-    t.left(60)
-    t.fd(ln)
-    t.right(120)
-    t.fd(ln)
-    t.left(60)
-    t.fd(ln)
-
 
 def draw_koch_segment(t, ln):
     if ln > 6:
@@ -36,5 +27,34 @@ def draw_koch_segment(t, ln):
         t.fd(ln)
 
 
+def hilbert(level, angle, step):
+    """
+    Метод рисующий кривую Гильберта
+    :param level: глубина кривой
+    :param angle: угол поворота
+    :param step: шаг
+    :return:
+    """
+    if level == 0:
+        return
+
+    t.right(angle)
+    hilbert(level - 1, -angle, step)
+
+    t.forward(step)
+    t.left(angle)
+    hilbert(level - 1, angle, step)
+
+    t.forward(step)
+    hilbert(level - 1, angle, step)
+
+    t.left(angle)
+    t.forward(step)
+    hilbert(level - 1, -angle, step)
+    t.right(angle)
+
+
+
 if __name__ == '__main__':
-    draw_koch_segment(t, 20000)
+    # draw_koch_segment(t, 20000)
+    hilbert(12, 90, 20)
