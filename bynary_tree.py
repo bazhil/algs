3d10801bd5199522480c0df2419c2cfef0b870d7
bazhil <i-bazh@mail.ru>
2021-12-25 20:35:21 +0300

    Add find_node method (not ideal)

--- bynary_tree.py
+++ bynary_tree.py
@@ -10,8 +10,8 @@ class BinaryNode:
     Узел бинарного дерева
     """
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, value):
+        self.value = value
         self.left_child = None
         self.right_child = None
 
@@ -22,12 +22,12 @@ class BinaryNode:
         :param node: узел дерева
         :return:
         """
-        print(f'Обработка вершины дерева {node.name}')
+        print(f'Обработка вершины дерева {node.value}')
         if node.left_child:
-            print(f'name of left_child is {node.left_child.name}')
+            print(f'value of left_child is {node.left_child.value}')
             self.traverse_preorder(node.left_child)
         if node.right_child:
-            print(f'name of right_child is {node.right_child.name}')
+            print(f'value of right_child is {node.right_child.value}')
             self.traverse_preorder(node.right_child)
 
 
@@ -39,11 +39,11 @@ class BinaryNode:
         :return:
         """
         if node.left_child:
-            print(f'name of left_child is {node.left_child.name}')
+            print(f'value of left_child is {node.left_child.value}')
             self.traverse_inorder(node.left_child)
-        print(f'Обработка вершины дерева {node.name}')
+        print(f'Обработка вершины дерева {node.value}')
         if node.right_child:
-            print(f'name of right_child is {node.right_child.name}')
+            print(f'value of right_child is {node.right_child.value}')
             self.traverse_inorder(node.right_child)
 
 
@@ -54,12 +54,12 @@ class BinaryNode:
         :return:
         """
         if node.left_child:
-            print(f'name of left_child is {node.left_child.name}')
+            print(f'value of left_child is {node.left_child.value}')
             self.traverse_postoder(node.left_child)
         if node.right_child:
-            print(f'name of right_child is {node.right_child.name}')
+            print(f'value of right_child is {node.right_child.value}')
             self.traverse_postoder(node.right_child)
-        print(f'Обработка вершины дерева {node.name}')
+        print(f'Обработка вершины дерева {node.value}')
 
 
     def traverse_depth_first(self, root):
@@ -77,17 +77,31 @@ class BinaryNode:
         while children.values:
             # получаю следующую вершину в очереди
             node = children.pop()
-            print(f'Обработка вершины дерева {node.name}')
+            print(f'Обработка вершины дерева {node.value}')
 
             if node.left_child:
-                print(f'name of left_child is {node.left_child.name}')
+                print(f'value of left_child is {node.left_child.value}')
                 children.push(node.left_child)
             if node.right_child:
-                print(f'name of right_child is {node.right_child.name}')
+                print(f'value of right_child is {node.right_child.value}')
                 children.push(node.right_child)
 
 
-
+    def find_node(self, target):
+        """
+        Метод поиска конкретной вершины по значению
+        :param target: значение для поиска вершины
+        :return:
+        """
+        # если нашли целевое значение, возвращаем вершину
+        if target == self.value:
+            print(f'Значение {target} найдено в узле {self.__str__()}')
+            return self.value
+        # смотрим, где целевое значение, в левом или правом поддереве
+        if target < self.value:
+            return None if not self.left_child else self.left_child.find_node(target)
+        else:
+            return None if not self.right_child else self.right_child.find_node(target)
 
 class TreeNode:
     """
@@ -117,14 +131,14 @@ class Branch:
 
 if __name__ == '__main__':
     # создаю дерево
-    root = BinaryNode("4")
-    node1 = BinaryNode("1")
-    node2 = BinaryNode("2")
-    node3 = BinaryNode("3")
-    node5 = BinaryNode("5")
-    node6 = BinaryNode("6")
-    node7 = BinaryNode("7")
-    node8 = BinaryNode("8")
+    root = BinaryNode(4)
+    node1 = BinaryNode(1)
+    node2 = BinaryNode(2)
+    node3 = BinaryNode(3)
+    node5 = BinaryNode(5)
+    node6 = BinaryNode(6)
+    node7 = BinaryNode(7)
+    node8 = BinaryNode(8)
 
     root.left_child = node2
     root.right_child = node5
@@ -138,4 +152,5 @@ if __name__ == '__main__':
     # root.traverse_preorder(root)
     # root.traverse_inorder(root)
     # root.traverse_postoder(root)
-    root.traverse_depth_first(root)
+    # root.traverse_depth_first(root)
+    root.find_node(8)
