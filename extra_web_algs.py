eac7e6bb3da355ae835e0ddbab9907434e158f24
bazhil <i-bazh@mail.ru>
2022-03-03 01:34:27 +0300

    add alg for get linked elements

--- extra_web_algs.py
+++ extra_web_algs.py
@@ -22,3 +22,32 @@ def extend_partial_ordering(node):
         # помещаю узел в итоговый список
         ordered_nodes.append(ready_node)
         extend_partial_ordering(node)
+
+
+def upgrated_extend_partial_ordering(node):
+    """
+    Усовершенствованное упорядочивание
+    :return:
+    """
+    # список узлов без предварительных условий
+    ready = []
+
+    for node in node.neighbors:
+        # находим узел без предварительных требований
+        ready_node = node.neighbors.pop()
+        if ready_node:
+            ready.append(ready_node)
+    # пока в списке ready содержатся элементы
+    while ready:
+        node = ready.pop()
+        ordered_nodes.append(node)
+
+        for link in node.links:
+            # делаем вид, что появилось новое свойство - количество элементов перед текущим
+            link.nodes[0].num_before_me = link.nodes[0].num_before_me - 1
+
+            if link.nodes[0].num_before_me == 0:
+                ready.append(link.nodes[0])
+    trigger = [node for node in ready if node.num_before_me == 0]
+    if any(trigger):
+        return None
