613660d3e685b2e278bf23cea1c5ab5bb5d42db8
bazhil <i-bazh@mail.ru>
2022-03-17 02:45:09 +0300

    Add first string alg

--- string_algs.py
+++ string_algs.py
@@ -2,3 +2,23 @@
 """
 Файл со строковыми алгоритмами из книги
 """
+
+def is_properly_nested(expression: str) -> bool:
+    """
+    Проверка корректного закрытия скобок
+    :param expression: выражение
+    :return:
+    """
+    counter = 0
+    for symbol in expression:
+        if symbol == '(':
+            counter -= 1
+        elif symbol == ')':
+            counter += 1
+    return bool(counter == 0)
+
+
+if __name__ == '__main__':
+    print(is_properly_nested('()'))
+    print(is_properly_nested('(()'))
+    print(is_properly_nested('())'))
