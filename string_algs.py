0d19bc459b3efc9ad20c9d75789955b3e6a23b1b
bazhil <i-bazh@mail.ru>
2022-03-22 00:49:54 +0300

    Add first string alg

--- string_algs.py
+++ string_algs.py
@@ -18,6 +18,28 @@ def is_properly_nested(expression: str) -> bool:
     return bool(counter == 0)
 
 
+def find_target(text, target):
+    """
+    Метод, который ищет подстроку в строке (нужно отладить!!!)
+    :param text: строка
+    :param target: подстрока
+    :return:
+    """
+
+    for i in range(len(text)):
+        found = True
+        # проверяем, начинается ли подстрока с позиции i
+        for j in range(len(target)):
+            # TODO: тут внимательнее с индексами!
+            if text[i] != target[j]:
+                found = False
+        # если нашли - возвращаем позицию подстроки в тексте
+        if found:
+            return i
+
+    return -1
+
+
 if __name__ == '__main__':
     print(is_properly_nested('()'))
     print(is_properly_nested('(()'))
